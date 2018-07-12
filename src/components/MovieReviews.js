d359ded5794e93731862d8b7f774f47215517703
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 14:17:58 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -2,7 +2,7 @@ class MovieReviews extends React.Component {
   render() {
     return (
       <div className="review-list">
-        this.props.reviews.map( review => <div className="review">review</div> ).join('')
+        this.props.reviews.map( review => <div className="review">review</div> )
       </div>
     );
   }
