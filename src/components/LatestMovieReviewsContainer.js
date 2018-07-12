5af45c45e1b2775404174d9eb7a1f16293cb5718
R. Harris Haynes <harrishaynes@gmail.com>
2018-07-12 13:34:53 +0000

    automatically backed up by learn

--- src/components/LatestMovieReviewsContainer.js
+++ src/components/LatestMovieReviewsContainer.js
@@ -6,3 +6,12 @@ const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/all.json?'
             + `api-key=${NYT_API_KEY}`;
 
 // Code LatestMovieReviewsContainer Here
+class LatestMovieReviewsContainer extends React.Component {
+  constructor() {
+    //code
+  }
+
+  render() {
+    return ();
+  }
+}
